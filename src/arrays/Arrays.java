9a0886123d00d22f30d7a216e04430618b74d63b
Admin <Admin@DESKTOP-1L1HD6G>
2024-01-04 09:03:40 +0530

    commit

--- src/arrays/Arrays.java
+++ src/arrays/Arrays.java
@@ -4,7 +4,29 @@ public class Arrays {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int []student=new int[6];
+		int []rollno=new int[10];
+		rollno[0]=1;
+		rollno[1]=2;
+		rollno[2]=3;
+		rollno[3]=4;
+		rollno[4]=5;
+		rollno[5]=6;
+		rollno[6]=7;
+		rollno[7]=8;
+		rollno[8]=9;
+		rollno[9]=10;
+		//rollno[10]=11;
+		for(int rolls:rollno) {
+			System.out.println(rolls);
+		}
+		//System.out.println(rollno);
+		System.out.println(rollno.length);
+		//for(int i=0;i<=rollno.length;i++) {
+			//System.out.println(rollno[i]);
+		//}
+		
+		
+		
 		
 
 	}
---
+++ src/arrays/Declarationarray.java
@@ -0,0 +1,28 @@
+package arrays;
+
+import java.util.List;
+
+public class Declarationarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//String str="hello world";
+		//String[]multiple=str.split("");
+		//multiple[1]="hello";
+		//multiple[0]="world";
+		//System.out.println(str);
+		String []names= {"sairam","ravi","pavani","vinod"};
+        names[2]="sai";
+		for(String str:names) {
+			System.out.println(str);
+		}
+		//list li=new ArrayList();
+		//li.add("sairam");
+		//li.add(120);
+		//System.out.println(li);
+		
+		
+
+	}
+
+}
